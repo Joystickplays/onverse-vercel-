ed89c30f59decbc458fa05425477a8d690a75a47
Joystickplays <joystickgaming09@gmail.com>
2024-07-17 22:00:40 +0700

    okay

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import "@/styles/globals.css"
+import { Button } from "@/components/ui/button"
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>Hello.</p>
+      <Button variant="destructive">I love you.</Button>
     </main>
   );
 }
